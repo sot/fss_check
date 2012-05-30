a25d42faa7b80759a15da9702ea25519ff13c133
Tom Aldcroft <taldcroft@gmail.com>
2012-05-30 15:54:39 +0000

    Significant overhaul

--- check_fss.py
+++ check_fss.py
@@ -1,90 +1,103 @@
+from itertools import count
+
+import matplotlib.pyplot as plt
+import numpy as np
+from Ska.Matplotlib import plot_cxctime
 import Ska.engarchive.fetch_eng as fetch
 
-bad_times = ['2000:048:00:00:00.000 2000:050:00:00:00.000',
-             '2000:070:23:00:00.000 2000:071:13:00:00.000',
-             '2001:110:00:00:00.000 2001:113:00:00:00.000',
-             '2003:200:00:00:00.000 2003:201:00:00:00.000',
-             '2004:315:00:00:00.000 2004:317:00:00:00.000',
-             '2008:225:00:00:00.000 2008:229:00:00:00.000',
-             '2008:292:00:00:00.000 2008:296:00:00:00.000',
-             '2010:149:00:00:00.000 2010:153:00:00:00.000',
-             '2011:187:12:00:00.000 2011:192:04:00:00.000',
-             '2011:299:00:00:00.000 2011:300:12:00:00.000',
-             '2002:031:16:09:58.434 2002:031:16:20:19.959',
-             '2002:219:02:12:33.194 2002:219:02:22:51.644',
-             '2002:229:16:49:34.857 2002:229:16:59:39.982',
-             '2003:211:01:19:00.287 2003:211:01:29:09.512',
-             '2003:219:00:13:37.167 2003:219:00:24:12.017',
-             '2004:018:08:34:20.529 2004:018:08:44:46.154',
-             '2004:034:05:51:58.039 2004:034:06:02:18.539',
-             '2004:203:00:50:41.357 2004:203:01:01:27.482',
-             '2006:199:07:25:17.155 2006:199:07:36:22.755',
-             '2007:077:14:18:39.114 2007:077:15:02:46.139',
-             '2007:011:21:08:51.203 2007:011:21:19:05.553',
-             '2007:175:10:41:12.351 2007:175:10:51:14.401',
-             '2007:193:23:30:34.028 2007:193:23:40:37.103',
-             '2008:003:20:10:30.456 2008:003:20:22:11.931',
-             '2008:003:20:12:15.006 2008:003:20:22:17.056',
-             '2008:037:22:24:30.474 2008:037:22:37:37.024',
-             '2008:164:19:46:43.357 2008:164:19:57:05.907',
-             '2008:208:21:06:16.138 2008:208:21:47:20.713',
-	     '2008:212:04:40:51.340 2008:212:05:21:23.115',
-	     '2008:214:21:10:40.491 2008:214:21:51:18.416',
-	     '2008:217:11:01:33.043 2008:217:11:41:36.118',
-	     '2008:220:20:33:52.544 2008:220:21:14:40.719',
-	     '2008:222:18:01:48.645 2008:222:18:41:55.820',
-	     '2008:227:22:11:33.723 2008:227:22:51:59.348',
-	     '2008:233:05:08:56.101 2008:233:05:50:36.551',
-	     '2008:235:23:12:45.827 2008:235:23:53:03.252',
-	     '2008:237:17:19:47.928 2008:237:18:00:13.553',
-	     '2008:238:14:53:13.753 2008:238:15:33:17.853',
-	     '2008:243:15:28:05.356 2008:243:16:08:09.456',
-	     '2008:243:15:29:14.031 2008:243:16:09:31.456',
-	     '2008:301:22:11:08.999 2008:301:22:21:13.099',
-             '2008:301:22:11:14.124 2008:301:22:21:15.149',
-             '2008:360:19:00:10.475 2008:360:19:10:55.575',
-   	     '2009:330:13:09:27.198 2009:330:13:20:13.323',
-   	     '2009:349:02:01:09.304 2009:349:02:12:17.979',
-   	     '2010:044:16:05:30.073 2010:044:16:48:10.998',
-	     '2011:153:01:20:24.336 2011:153:02:04:46.736',
-	     '2011:187:12:23:44.974 2011:187:12:34:04.097',
-   	     '2011:264:04:55:53.942 2011:264:05:38:17.442',
-   	     '2005:305:12:00:00.000 2005:305:13:00:00.000',
-   	     '2007:355:14:30:00.000 2007:355:15:30:00.000',   	     
-   	     '2011:182:05:00:00.000 2011:182:06:00:00.000',
-   	     '2003:221:16:00:00.000 2003:221:17:00:00.000',
-   	     '2002:250:15:30:00.000 2002:250:17:00:00.000',
-   	     '2008:037:22:00:00.000 2008:037:23:30:00.000']
+from bad_times import bad_times
 
-msids = ('aopssupm', 'aopcadmd', 'aoacaseq', 'pitch', 'roll',
-         'aoalpang', 'aobetang', 'aosunprs')
-print 'fetching data'
-x = fetch.Msidset(msids, '2006:001', '2012:144')
-print 'starting interpolate'
-x.interpolate(1.025)
+def make_plots(out, alpha_err_lim=5.0):
+    times= out['times']
+    pitch = out['pitch']
+    alpha_err = out['alpha'] - out['roll']
+    alpha_err1 = abs(alpha_err) > alpha_err_lim
 
-for msid in x.values():
-    print 'filter_bad_times', msid.msid
-    msid.filter_bad_times(table=bad_times)
+    for i, title, xlabel, ylabel in (
+        (1, 'Roll error vs. pitch', 'Pitch (deg)', 'Roll err (deg)'),
+        (2, 'Roll error vs. time', None, 'Roll err (deg)'),
+        (3, 'Glitch pitch vs. time', None, 'Pitch (deg)')):
+        plt.figure(i)
+        plt.clf()
+        plt.grid()
+        plt.title(title)
+        plt.ylabel(ylabel)
+        if xlabel:
+            plt.xlabel(xlabel)
 
-ok = ((x['pitch'].vals > 134) &
-      (x['pitch'].vals < 144))
+    zipvals = zip((~out['kalman'], out['kalman']),
+                  ('c', 'r'),
+                  ('b', 'r'))
+    for filt, col1, col2 in zipvals:
+        plt.figure(1)
+        aerr = alpha_err1 & filt
+        sperr = out['aosunprs'] & aerr
+        n_sperr = sum(sperr)
+        plt.plot(out['pitch'][aerr], alpha_err[aerr], ',', color=col1, mec=col1)
+        plt.plot(out['pitch'][::20], alpha_err[::20], ',', color=col1, mec=col1)
+        if n_sperr:
+            plt.plot(out['pitch'][sperr], alpha_err[sperr], '.',
+                     color=col2, mec=col2)
 
-nvals = np.sum(ok)
-colnames = ('times', 'pitch', 'roll', 'alpha', 'beta', 'aosunprs', 'aopssupm', 'kalman')
-dtypes = ('f8', 'f4', 'f4', 'f4', 'f4', 'bool', 'bool', 'bool')
-out = np.empty(nvals, dtype=zip(colnames, dtypes))
+        figure(2)
+        plot_cxctime(times[aerr], alpha_err[aerr], ',', color=col1, mec=col1)
+        if n_sperr:
+            plot_cxctime(times[sperr], alpha_err[sperr], '.',
+                         color=col2, mec=col2)
 
-out['times'][:] = x.times[ok]
-out['pitch'][:] = x['pitch'].vals[ok]
-out['roll'][:] = x['roll'].vals[ok]
-out['alpha'][:] = -x['aoalpang'].vals[ok]
-out['beta'][:] = 90 - x['aobetang'].vals[ok]
-out['aosunprs'][:] = x['aosunprs'].vals[ok] == 'SUN '
-out['aopssupm'][:] = x['aopssupm'].vals[ok] == 'ACT '
-out['kalman'][:] = ((x['aoacaseq'].vals[ok] == 'KALM') &
-                    (x['aopcadmd'].vals[ok] == 'NPNT'))
-np.save('fss2.npy', out)
+        figure(3)
+        plot_cxctime(times[aerr], pitch[aerr], ',', color=col1, mec=col1)
+        if n_sperr:
+            plot_cxctime(times[sperr], pitch[sperr], '.r',
+                         color=col2, mec=col2)
+
+    for i in range(1, 4):
+        plt.figure(i)
+        x0, x1 = plt.xlim()
+        dx = (x1 - x0) / 20
+        plt.xlim(x0 - dx, x1 + dx)
+
+def get_data(start='2005:001', stop='2012:144', interp=32.8,
+             pitch0=134, pitch1=144):
+    msids = ('aopssupm', 'aopcadmd', 'aoacaseq', 'pitch', 'roll',
+             'aoalpang', 'aobetang', 'aosunprs')
+    print 'fetching data'
+    x = fetch.MSIDset(msids, start, stop)
+    print 'starting interpolate'
+    x.interpolate(interp, filter_bad=False)
+
+    for msid in x.values():
+        print 'filter_bad_times', msid.msid
+        msid.filter_bad_times(table=bad_times)
+
+    ok = ((x['pitch'].vals > pitch0) &
+          (x['pitch'].vals < pitch1))
+
+    nvals = np.sum(ok)
+    bads = np.zeros(nvals, dtype=bool)
+    for msid in x.values():
+        print msid.msid, sum(msid.bads[ok])
+        bads = bads | msid.bads[ok]
+    ok[ok] = ok[ok] & ~bads
+
+    nvals = np.sum(ok)
+    colnames = ('times', 'pitch', 'roll', 'alpha', 'beta',
+                'aosunprs', 'aopssupm', 'kalman')
+    dtypes = ('f8', 'f4', 'f4', 'f4', 'f4', 'bool', 'bool', 'bool')
+    out = np.empty(nvals, dtype=zip(colnames, dtypes))
+
+    out['times'][:] = x.times[ok]
+    out['pitch'][:] = x['pitch'].vals[ok]
+    out['roll'][:] = x['roll'].vals[ok]
+    out['alpha'][:] = -x['aoalpang'].vals[ok]
+    out['beta'][:] = 90 - x['aobetang'].vals[ok]
+    out['aosunprs'][:] = x['aosunprs'].vals[ok] == 'SUN '
+    out['aopssupm'][:] = x['aopssupm'].vals[ok] == 'ACT '
+    out['kalman'][:] = ((x['aoacaseq'].vals[ok] == 'KALM') &
+                        (x['aopcadmd'].vals[ok] == 'NPNT'))
+    return out
+
+# np.save('fss4.npy', out)
 
 # pitch_err = pitch - pitch_fss
 # roll_err = roll - roll_fss
@@ -98,7 +111,7 @@ np.save('fss2.npy', out)
 # figure(3)
 # clf()
 # plot(pitch,roll_err, ',')
-# plot(pitch[sme], roll_err[sme], 'or', markeredgecolor='r')
+# plot(pitch[sme], roll_err[sme], 'or', mec='r')
 
 # bad = abs(roll_err) > 5
 # figure(4)
