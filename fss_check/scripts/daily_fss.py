20c248c90c86e3e2bb22959020bb0967040133bc
Tom Aldcroft <taldcroft@gmail.com>
2023-05-14 06:01:30 -0400

    Refactor main into smaller functions

--- fss_check/scripts/daily_fss.py
+++ fss_check/scripts/daily_fss.py
@@ -9,6 +9,7 @@ import astropy.units as u
 import jinja2
 import matplotlib
 import numpy as np
+from acdc.common import send_mail
 from astropy.table import vstack
 from cheta import fetch_eng as fetch
 from cxotime import CxoTime, CxoTimeLike
@@ -75,7 +76,13 @@ def get_parser():
         default=False,
         help="Use MAUDE to get most recent data",
     )
-    # Add --level argument to set logging level
+    parser.add_argument(
+        "--email",
+        action="append",
+        dest="emails",
+        default=[],
+        help="Email address for notification (multiple allowed)",
+    )
     parser.add_argument(
         "--level",
         default="INFO",
@@ -115,25 +122,97 @@ def main(args=None):
     }
 
     # Get data
-    logger.info(f"Processing {starts['long_term'].date} to {stop.date}")
-    get_data_func = get_fss_prim_data_cached if args.cache_data else get_fss_prim_data
-    dat = get_data_func(starts["long_term"], stop)
-    dat = add_pitch_roll_columns(dat)
-
-    # Extract a recent data subset
-    dats = {"long_term": dat}
-    for subset in ["recent", "table"]:
-        i0 = np.searchsorted(dat["times"], starts[subset].cxcsec)
-        dats[subset] = dat[i0:]
+    dats = get_datasets(args.cache_data, stop, starts)
 
     # Last available date in data
-    date_last = CxoTime(dat["times"][-1])
+    date_last = CxoTime(dats["recent"]["times"][-1])
     date_last_local_fmt = get_formatted_local_time(date_last)
 
     outdir = Path(args.out)
     outdir.mkdir(parents=True, exist_ok=True)
 
-    # Classic plot of points with large > 2 deg pitch or roll errors
+    # Classic plot of points with large > 2 deg pitch or roll errors near FSS FOV edge
+    large_pr_err_sun, large_pr_err_no_sun = process_large_pitch_roll_errors(
+        args, stop, starts, dats, outdir
+    )
+
+    pitch_roll_time_outfiles = process_pitch_roll_time_plots(
+        dats["recent"], outdir, large_pr_err_sun, large_pr_err_no_sun
+    )
+
+    # 2 x 2 grid of pitch/roll error vs pitch/roll for data within SPM pitch limit
+    plot_delta_vs_pitch_roll(
+        dats["recent"],
+        max_pitch=CONFIG["spm_pitch_limit"],
+        err_lim=CONFIG["plot_delta_vs_pitch_roll"]["err_lim"],
+        outfile=outdir / "delta_pitch_roll_vs_pitch_roll_recent.png",
+    )
+
+    plot_pitch_roll_spm_mp_constraints(
+        dats["recent"],
+        pitch_max=CONFIG["spm_pitch_limit"],
+        err_caution=CONFIG["plot_pitch_roll_spm_mp_constraints"]["err_caution"],
+        err_warning=CONFIG["plot_pitch_roll_spm_mp_constraints"]["err_warning"],
+        outfile=outdir / "pitch_roll_spm_mp_constraints_recent.png",
+    )
+
+    # Write out the HTML report
+    template = jinja2.Template((data_dir / "index.html").read_text())
+    context = {
+        "date_last": date_last.date[:-4],
+        "date_last_local": date_last_local_fmt,
+        "stop": stop.date[:-4],
+        "days_long_term": args.days_long_term,
+        "days_recent": args.days_recent,
+        "days_table": args.days_table,
+        "large_pitch_roll_error": large_pr_err_sun,
+        "large_prerr_no_sun": large_pr_err_no_sun,
+        "config": CONFIG,
+        "axes": ["pitch", "roll"],
+        "pitch_roll_time_outfiles": pitch_roll_time_outfiles,
+    }
+    txt = template.render(**context)
+    (outdir / "index.html").write_text(txt)
+
+    # Copy the historical plot files
+    outdir_hist = outdir / "fss_prim_hist"
+    outdir_hist.mkdir(parents=True, exist_ok=True)
+    for inpath in (data_dir / "fss_prim_hist").glob("*"):
+        outpath = outdir_hist / inpath.name
+        if not outpath.exists():
+            shutil.copy2(inpath, outpath)
+
+
+def process_pitch_roll_time_plots(dat, outdir, large_pr_err_sun, large_pr_err_no_sun):
+    pitch_roll_time_outfiles = []
+    pitch_roll_time_table = vstack(
+        [
+            large_pr_err_sun[: CONFIG["max_pitch_roll_time_plots"]],
+            large_pr_err_no_sun[: CONFIG["max_pitch_roll_time_plots"]],
+        ]
+    )
+
+    for interval in pitch_roll_time_table:
+        if not interval["recent"]:
+            continue
+        start = CxoTime(interval["datestart"]) - 5 * u.min
+        stop = CxoTime(interval["datestop"]) + 5 * u.min
+        outfile = outdir / f"pitch_roll_vs_time_{interval['datestart']}.png"
+        plot_roll_pitch_vs_time(
+            dat,
+            start,
+            stop,
+            pitch_max=CONFIG["spm_pitch_warning"],
+            plot_errs=CONFIG["plot_roll_pitch_vs_time"]["plot_errs"],
+            outfile=outfile,
+            suptitle=interval["datestart"],
+        )
+        pitch_roll_time_outfiles.append(outfile.name)
+
+    return pitch_roll_time_outfiles
+
+
+def process_large_pitch_roll_errors(args, stop, starts, dats, outdir):
     for i_epoch, epoch in enumerate(starts):
         start = starts[epoch]
         dat = dats[epoch]
@@ -175,72 +254,28 @@ def main(args=None):
     dt = stop - CxoTime(large_prerr_no_sun["datestart"])
     large_prerr_no_sun["recent"] = dt < args.highlight_recent_days * u.day
 
-    pitch_roll_time_outfiles = []
-    pitch_roll_time_table = vstack(
-        [
-            large_pr_error_sun[: CONFIG["max_pitch_roll_time_plots"]],
-            large_prerr_no_sun[: CONFIG["max_pitch_roll_time_plots"]],
-        ]
-    )
-    for interval in pitch_roll_time_table:
-        if not interval["recent"]:
-            continue
-        start = CxoTime(interval["datestart"]) - 5 * u.min
-        stop = CxoTime(interval["datestop"]) + 5 * u.min
-        outfile = outdir / f"pitch_roll_vs_time_{interval['datestart']}.png"
-        plot_roll_pitch_vs_time(
-            dat,
-            start,
-            stop,
-            pitch_max=CONFIG["spm_pitch_warning"],
-            plot_errs=CONFIG["plot_roll_pitch_vs_time"]["plot_errs"],
-            outfile=outfile,
-            suptitle=interval["datestart"],
-        )
-        pitch_roll_time_outfiles.append(outfile.name)
+    return large_pr_error_sun, large_prerr_no_sun
 
-    # 2 x 2 grid of pitch/roll error vs pitch/roll for data within SPM pitch limit
-    plot_delta_vs_pitch_roll(
-        dats["recent"],
-        max_pitch=CONFIG["spm_pitch_limit"],
-        err_lim=CONFIG["plot_delta_vs_pitch_roll"]["err_lim"],
-        outfile=outdir / "delta_pitch_roll_vs_pitch_roll_recent.png",
-    )
 
-    outfile = outdir / "pitch_roll_spm_mp_constraints_recent.png"
-    plot_pitch_roll_spm_mp_constraints(
-        dats["recent"],
-        pitch_max=CONFIG["spm_pitch_limit"],
-        err_caution=CONFIG["plot_pitch_roll_spm_mp_constraints"]["err_caution"],
-        err_warning=CONFIG["plot_pitch_roll_spm_mp_constraints"]["err_warning"],
-        outfile=outfile,
-    )
+def get_datasets(cache_data, stop, starts):
+    logger.info(f"Processing {starts['long_term'].date} to {stop.date}")
+    get_data_func = get_fss_prim_data_cached if cache_data else get_fss_prim_data
+    dat = get_data_func(starts["long_term"], stop)
+    dat = add_pitch_roll_columns(dat)
 
-    # Write out the HTML report
-    template = jinja2.Template((data_dir / "index.html").read_text())
-    context = {
-        "date_last": date_last.date[:-4],
-        "date_last_local": date_last_local_fmt,
-        "stop": stop.date[:-4],
-        "days_long_term": args.days_long_term,
-        "days_recent": args.days_recent,
-        "days_table": args.days_table,
-        "large_pitch_roll_error": large_pr_error_sun,
-        "large_prerr_no_sun": large_prerr_no_sun,
-        "config": CONFIG,
-        "axes": ["pitch", "roll"],
-        "pitch_roll_time_outfiles": pitch_roll_time_outfiles,
-    }
-    txt = template.render(**context)
-    (outdir / "index.html").write_text(txt)
+    # Extract a recent data subset
+    dats = {"long_term": dat}
+    for subset in ["recent", "table"]:
+        i0 = np.searchsorted(dat["times"], starts[subset].cxcsec)
+        dats[subset] = dat[i0:]
+    return dats
 
-    # Copy the historical plot files
-    outdir_hist = outdir / "fss_prim_hist"
-    outdir_hist.mkdir(parents=True, exist_ok=True)
-    for inpath in (data_dir / "fss_prim_hist").glob("*"):
-        outpath = outdir_hist / inpath.name
-        if not outpath.exists():
-            shutil.copy2(inpath, outpath)
+
+def send_alert_email(opt, logger):
+    subject = f"fss_check3: large pitch/roll error"
+    lines = ["Long drop interval(s) found for the following perigee events:"]
+    text = "\n".join(lines)
+    send_mail(logger, opt, subject, text, __file__)
 
 
 def get_formatted_local_time(date: CxoTimeLike):
